128b565cb419bd5c369b7f75deaf928e0573d69e
Probotmaker8 <101275268+Probotmaker8@users.noreply.github.com>
2022-05-20 16:26:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -875,7 +875,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.edit_text(
             text=script.ABOUT_TXT.format(temp.B_NAME),
             reply_markup=reply_markup,
-            parse_mode='html'
+            parse_mode='html',
         )
     elif query.data == "shazam":
         buttons = [[
