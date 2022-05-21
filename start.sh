8d5d8e6326b87d3a128a6302faa5e7911de20d70
Probotmaker8 <101275268+Probotmaker8@users.noreply.github.com>
2022-05-21 07:48:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/4kcinemas/My-Dream.git /My-Dream
+  git clone https://github.com/Probotmaker8/stella-bot/stella-bot.git /stella-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /My-Dream
+  git clone $UPSTREAM_REPO /stella-bot
 fi
-cd /My-Dream
+cd /stella-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
