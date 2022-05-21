dfc3d60499719b407136fcd26669a23f482db5bf
Probotmaker8 <101275268+Probotmaker8@users.noreply.github.com>
2022-05-21 08:14:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -423,6 +423,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await msg.delete()
 
             del msg1, msg
+
     elif query.data.startswith("checksub"):
         if AUTH_CHANNEL and not await is_subscribed(client, query):
             await query.answer("I Like Your Smartness, But Don't Be Oversmart Okay", show_alert=True)
